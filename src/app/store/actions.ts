f39434a3dc2ec89099c394095926ce94cf2ea552
elbarqy <elbarqy.dev@gmail.com>
2022-08-30 16:59:39 +0200

    store

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -1,12 +1,17 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 
 export enum StoreActions {
   INCREMENT = '[COUNTER Component] Increment',
   DECREMENT = '[COUNTER Component] Decrement',
-  RESET = "[COUNTER Component] Reset"
+  RESET = '[COUNTER Component] Reset',
+  WITH_PROPS = '[WITH PROPS] NULL',
 }
 
 export const incrementAction = createAction(StoreActions.INCREMENT);
 export const decrementAction = createAction(StoreActions.DECREMENT);
 export const resetAction = createAction(StoreActions.RESET);
 
+export const withPropsAction = createAction(
+  StoreActions.WITH_PROPS,
+  props<{ value: number }>()
+);
--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -1,4 +1,9 @@
-import { incrementAction, decrementAction, resetAction } from './actions';
+import {
+  incrementAction,
+  decrementAction,
+  resetAction,
+  withPropsAction,
+} from './actions';
 import { createReducer, on } from '@ngrx/store';
 export const initialState = 0;
 
@@ -6,5 +11,6 @@ export const counterReducer = createReducer(
   initialState,
   on(incrementAction, (state) => state + 1),
   on(decrementAction, (state) => state - 1),
-  on(resetAction, (state) => 0)
+  on(resetAction, (state) => 0),
+  on(withPropsAction, (state, action) => action.value)
 );
